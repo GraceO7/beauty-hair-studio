1503b69bbf741af9ef431e492f78e598719f28ba
Ojeyemi Opeyemi <161251930+GraceO7@users.noreply.github.com>
2025-05-30 16:35:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,20 +7,4 @@ function hideMenubar(){
     menubar.style.display = 'none'
 };
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    const sections = document.querySelectorAll('section');
-    const fadeIn = (entries, observer)=>{
-        entries.forEach(entry=>{
-            if(entry.isIntersecting){
-                entry.target.classList.add('fade-in');
-        observer.unobserve(entry.target);
-            }
-        });
-        };
-        const observer = new IntersectionObserver(fadeIn,{
-            threshold : 0.2
-        });
-        sections.forEach(section=>{
-            observer.observe(section)
-        })
-})
+
